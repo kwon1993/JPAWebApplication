f22d0a003a3e49b14406bd613536e007081a34db
kwon oyeop <wdmono@naver.com>
2021-05-07 17:33:10 +0900

    지연 로딩과 조회 성능 최적화

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -87,4 +87,9 @@ public class OrderRepository {
         TypedQuery<Order> query = em.createQuery(criteriaQuery).setMaxResults(1000);
         return query.getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery("select o from Order o join fetch o.member m join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
 }
